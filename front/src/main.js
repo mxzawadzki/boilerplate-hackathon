8f08fe2925524957576c2f6a98d7bd1e280194e7
Adam D Publicis <adam.dabrowski@publicis.com>
2019-10-09 16:25:50 +0200

    Add firebase to boilerplate

--- front/src/main.js
+++ front/src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
+import firebase from 'firebase';
 import App from './App.vue'
 import store from './store'
 import router from './router'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SEND_ID"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   store,
