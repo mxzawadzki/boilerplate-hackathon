906989b27ee87b100887ccee082f92e386063c0f
Marcin Zawadzki <marcin.zawadzki@saatchiis.com>
2019-10-12 19:55:39 +0200

    add userbar wip

--- front/src/components/UserBar.vue
+++ front/src/components/UserBar.vue
@@ -1,23 +1,50 @@
 <template>
   <div class="userbar">
-    <h1>Hello</h1>
+    <v-btn depressed color="primary" @click.stop="dialog = true">Login</v-btn>
+    <v-btn depressed color="primary">Menu</v-btn>
+    <v-dialog
+        v-model="dialog"
+        max-width="290"
+      >
+        <v-card>
+          <v-card-title class="headline">Login</v-card-title>
+  
+          <v-card-text>
+            Lorem ipsum dolor sit amet.
+          </v-card-text>
+  
+          <v-card-actions>
+            <div class="flex-grow-1"></div>
+            <v-btn
+              color="green darken-1"
+              text
+              @click="dialog = false"
+            >
+              Login
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      dialog: false,
+    }
+  }
 }
 </script>
 
 <style>
 .userbar {
-  width: 100px;
-  height: 50px;
+  display: flex;
   position: fixed;
   top: 50px;
   right: 50px;
-  background-color: #252525;
+  z-index: 1000;
 }
 
   h1 {
