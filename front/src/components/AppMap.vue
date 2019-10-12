a828615d97972cdbb70ee82711c7de1e7777d402
Adam Buczek <adam.buczek@publicis.com>
2019-10-12 16:47:14 +0200

    add api an d geol

--- front/src/components/AppMap.vue
+++ front/src/components/AppMap.vue
@@ -1,29 +1,33 @@
 <template>
   <l-map
-      style="height: 100vh; width: 100vw"
-      :zoom="zoom"
-      :center="center"
-      @update:zoom="zoomUpdated"
-      @update:center="centerUpdated"
-      @update:bounds="boundsUpdated"
+    style="height: 100vh; width: 100vw"
+    :zoom="zoom"
+    :center="center"
+    @update:zoom="zoomUpdated"
+    @update:center="centerUpdated"
+    @update:bounds="boundsUpdated"
+  >
+    <l-tile-layer :url="url"></l-tile-layer>
+    <l-marker
+      v-for="marker in markers"
+      :key="marker.id"
+      :lat-lng="marker.geometry.coordinates"
+      @click="showPopup"
     >
-      <l-tile-layer :url="url"></l-tile-layer>
-      <l-marker v-for="marker in markers" :key="marker.id" :lat-lng="marker.geometry.coordinates" @click="showPopup">
-        <l-popup>{{marker.properties.popupContent}}</l-popup>
-        <l-icon
-          :icon-anchor="staticAnchor"
-          class-name="someExtraClass">
-          <div class="headline">{{ customText }}</div>
-          <img src="@/assets/img/bottle.png">
-        </l-icon>
-      </l-marker>
-    </l-map>
+      <l-popup>{{marker.properties.popupContent}}</l-popup>
+      <l-icon :icon-anchor="staticAnchor" class-name="someExtraClass">
+        <div class="headline">{{ customText }}</div>
+        <img src="@/assets/img/bottle.png" />
+      </l-icon>
+    </l-marker>
+  </l-map>
 </template>
 
 <script>
-import {LMap, LTileLayer, LMarker, LPopup, LIcon } from 'vue2-leaflet'
+import { getPointsForBounds } from "@/utils/api.js";
+import { LMap, LTileLayer, LMarker, LPopup, LIcon } from "vue2-leaflet";
 export default {
-  name: 'AppMap',
+  name: "AppMap",
   components: {
     LMap,
     LTileLayer,
@@ -31,58 +35,59 @@ export default {
     LPopup,
     LIcon
   },
-  data () {
+  data() {
     return {
-      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
       icon: L.icon({
-        iconUrl: require('@/assets/img/bottle.png'),
+        iconUrl: require("@/assets/img/bottle.png"),
         iconSize: [32, 37],
         iconAnchor: [16, 37]
       }),
       staticAnchor: [16, 37],
-      customText: 'Bottle Drop',
+      customText: "Bottle Drop",
       iconSize: 64,
-      zoom: 3,
-      center: [52.2297, 21.0122],
+      zoom: 16,
+      center: [52.211900643211195, 20.99944747210445],
       markers: [
         {
-          "type": "Feature",
-          "properties": {
-            "name": "Coors Field",
-            "amenity": "Baseball Stadium",
-            "popupContent": "This is where the Rockies play!"
+          type: "Feature",
+          properties: {
+            name: "Coors Field",
+            amenity: "Baseball Stadium",
+            popupContent: "This is where the Rockies play!"
           },
-          "geometry": {
-            "type": "Point",
-            "coordinates": [52.2297, 21.0122]
+          geometry: {
+            type: "Point",
+            coordinates: [52.2297, 21.0122]
           }
         },
         {
-          "type": "Feature",
-          "properties": {
-            "name": "Coors Field",
-            "amenity": "Baseball Stadium",
-            "popupContent": "This is where the Rockies play!"
+          type: "Feature",
+          properties: {
+            name: "Coors Field",
+            amenity: "Baseball Stadium",
+            popupContent: "This is where the Rockies play!"
           },
-          "geometry": {
-            "type": "Point",
-            "coordinates": [51.2297, 20.0122]
+          geometry: {
+            type: "Point",
+            coordinates: [51.2297, 20.0122]
           }
         }
       ]
-    }
+    };
   },
   methods: {
     showPopup(e) {
-      console.log(e)
+      console.log(e);
     },
-    zoomUpdated (zoom) {
+    zoomUpdated(zoom) {
       this.zoom = zoom;
     },
-    centerUpdated (center) {
+    centerUpdated(center) {
       this.center = center;
     },
-    boundsUpdated (bounds) {
+    boundsUpdated(bounds) {
+      getPointsForBounds(bounds);
       this.bounds = bounds;
     }
   },
@@ -119,10 +124,9 @@ export default {
         );
       };
     }
-  },
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
