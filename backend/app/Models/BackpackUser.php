7f7454dd33f6e240aa63b168119e863d863287a5
Hubert <badocha.hubert@gmail.com>
2019-10-12 19:47:19 +0200

    lowercas

--- backend/app/Models/BackpackUser.php
+++ backend/app/Models/BackpackUser.php
@@ -7,6 +7,36 @@ use Illuminate\Notifications\Notifiable;
 use Backpack\CRUD\app\Models\Traits\InheritsRelationsFromParentModel;
 use Backpack\CRUD\app\Notifications\ResetPasswordNotification as ResetPasswordNotification;
 
+/**
+ * App\Models\BackpackUser
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property int $score
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property int $is_admin
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser whereIsAdmin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser whereScore($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\BackpackUser whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class BackpackUser extends User
 {
     use InheritsRelationsFromParentModel;
