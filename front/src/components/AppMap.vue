e9c2d27cbcd7b922ced789d25eac20d419c35e75
Marcin Zawadzki <marcin.zawadzki@saatchiis.com>
2019-10-12 11:47:46 +0200

    add map layer

--- front/src/components/AppMap.vue
+++ front/src/components/AppMap.vue
@@ -23,7 +23,7 @@ export default {
   },
   data () {
     return {
-      // url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       zoom: 3,
       center: [47.413220, -1.219482],
       markerLatLng: [47.313220, -1.319482]
