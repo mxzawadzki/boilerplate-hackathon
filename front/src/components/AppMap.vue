df7e0473a2e8fc0e7502902f36ae9da693c02024
Marcin Zawadzki <marcin.zawadzki@saatchiis.com>
2019-10-12 11:31:09 +0200

    add map component

---
+++ front/src/components/AppMap.vue
@@ -0,0 +1,37 @@
+<template>
+  <l-map
+      style="height: 80%; width: 100%"
+      :zoom="zoom"
+      :center="center"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+      @update:bounds="boundsUpdated"
+    >
+      <l-tile-layer :url="url"></l-tile-layer>
+      <l-marker :lat-lng="markerLatLng" ></l-marker>
+    </l-map>
+</template>
+
+<script>
+import {LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+export default {
+  name: 'AppMap',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  data () {
+    return {
+      // url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      zoom: 3,
+      center: [47.413220, -1.219482],
+      markerLatLng: [47.313220, -1.319482]
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
