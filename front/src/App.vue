1116045f2feeffb2a89fca60bdc35cfbe04f105f
Damian Jamka <damianjamka@gmail.com>
2019-10-12 17:01:42 +0200

    test

--- front/src/App.vue
+++ front/src/App.vue
@@ -4,13 +4,51 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+
+    <button @click="AuthProvider('google')">Login GOOGLE</button>
+    <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    AuthProvider(provider) {
+      console.log('dupa');
+
+    console.log(this.$auth);
+      // this.$auth
+      //   .authenticate(provider)
+      //   .then(response => {
+      //     // this.SocialLogin(provider, response);
+      //   })
+      //   .catch(err => {
+      //     console.log({ err: err });
+      //   });
+    },
+
+    SocialLogin(provider, response) {
+      console.log('siema');
+      // this.$http
+      //   .post("/sociallogin/" + provider, response)
+      //   .then(response => {
+      //     console.log(response.data);
+      //   })
+      //   .catch(err => {
+      //     console.log({ err: err });
+      //   });
+    }
+  }
+};
+</script>
+
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- front/src/main.js
+++ front/src/main.js
@@ -1,20 +1,30 @@
-import Vue from 'vue'
-import firebase from 'firebase';
-import App from './App.vue'
-import store from './store'
-import router from './router'
-import { Icon }  from 'leaflet'
+import Vue from "vue";
+import firebase from "firebase";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import { Icon } from "leaflet";
+import VueSocialauth from "vue-social-auth";
 // import 'leaflet/dist/leaflet.css'
+
+Vue.use(VueSocialauth, {
+  providers: {
+    google: {
+      clientId: "324788647488-tcadv6ov0o139k3d8pm3enmj950d45k4.apps.googleusercontent.com",
+      redirectUri: "/auth/github/callback" // Your client app URL
+    }
+  }
+});
 
 delete Icon.Default.prototype._getIconUrl;
 
 Icon.Default.mergeOptions({
-  iconRetinaUrl: require('@/assets/leaflet/marker-icon-2x.png'),
-  iconUrl: require('@/assets/leaflet/marker-icon.png'),
-  shadowUrl: require('@/assets/leaflet/marker-shadow.png')
+  iconRetinaUrl: require("@/assets/leaflet/marker-icon-2x.png"),
+  iconUrl: require("@/assets/leaflet/marker-icon.png"),
+  shadowUrl: require("@/assets/leaflet/marker-shadow.png")
 });
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
@@ -25,10 +35,12 @@ const firebaseConfig = {
   messagingSenderId: "YOUR_MESSAGING_SEND_ID"
 };
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   store,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
+
+
