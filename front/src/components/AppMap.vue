4a7e327e451a55b97b68f5eb5973db6ee66f2eb4
Marcin Zawadzki <marcin.zawadzki@saatchiis.com>
2019-10-12 12:29:10 +0200

    add first geojson

--- front/src/components/AppMap.vue
+++ front/src/components/AppMap.vue
@@ -8,7 +8,20 @@
       @update:bounds="boundsUpdated"
     >
       <l-tile-layer :url="url"></l-tile-layer>
+      <!-- <l-marker v-for="marker in markers" :key="marker.id"></l-marker> -->
       <l-marker :lat-lng="markerLatLng" ></l-marker>
+      <l-marker :lat-lng="[47.413220, -1.189482]">
+        <l-icon
+          :icon-anchor="staticAnchor"
+          class-name="someExtraClass">
+          <div class="headline">{{ customText }}</div>
+          <img src="static/images/layers.png">
+        </l-icon>
+      </l-marker>
+      <l-geo-json
+        :geojson="geojson"
+      >
+      </l-geo-json>
     </l-map>
 </template>
 
@@ -26,7 +39,15 @@ export default {
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       zoom: 3,
       center: [52.2297, 21.0122],
-      markerLatLng: [52.2297, 21.0122]
+      markerLatLng: [52.2297, 21.0122],
+      geojson: {     "type": "Feature",     "properties": {         "name": "Coors Field",         "amenity": "Baseball Stadium",         "popupContent": "This is where the Rockies play!"     },     "geometry": {         "type": "Point",         "coordinates": [-104.99404, 39.75621]     } },
+      markers: [
+        {
+          id: '',
+          markerLating: '',
+
+        }
+      ]
     }
   },
   methods: {
