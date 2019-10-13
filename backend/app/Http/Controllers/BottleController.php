31f0a1dd26614ad9a2edcd654762855c14942ece
Hubert <badocha.hubert@gmail.com>
2019-10-13 12:29:18 +0200

    fix

--- backend/app/Http/Controllers/BottleController.php
+++ backend/app/Http/Controllers/BottleController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Resource\BottleResource;
+use App\Http\Resource\BottleResource;
 use App\Bottle;
 
 class BottleController extends Controller
