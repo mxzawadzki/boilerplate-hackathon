30c5d1c05fd37351cda24e3b745006638f2630ae
Hubert <badocha.hubert@gmail.com>
2019-10-13 11:20:10 +0200

    crud for qrs

--- backend/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ backend/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,3 +2,4 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class=nav-item><a class=nav-link href="{{ backpack_url('elfinder') }}"><i class="nav-icon fa fa-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('place') }}'><i class='nav-icon fa fa-question'></i> Places</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('qr') }}'><i class='nav-icon fa fa-question'></i> Qrs</a></li>
--- backend/routes/backpack/custom.php
+++ backend/routes/backpack/custom.php
@@ -12,4 +12,5 @@ Route::group([
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
     Route::crud('place', 'PlaceCrudController');
+    Route::crud('qr', 'QrCrudController');
 }); // this should be the absolute last line of this file
